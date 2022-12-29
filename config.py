72b744fca36cf8fb9069310e0606230afdd123e2
kbyong0905 <108616995+kbyong0905@users.noreply.github.com>
2022-12-29 20:03:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cq9tnmwshojv.us-east-1.rds.amazonaws.com"
+customhost = "employee.c9tlv938h2zq.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "kongbangyong-employee"
+custombucket = "kongbangyong-bucket"
 customregion = "us-east-1"
